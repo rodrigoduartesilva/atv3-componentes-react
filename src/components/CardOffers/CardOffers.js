f2ae9cb691196b26fbee0e59f2de9902276a01ae
Rodrigo Duarte <rodrigoduartesilva@yahoo.com.br>
2024-09-27 23:54:36 -0300

    feat: implementacao da funcao promoCards para insercao do objeto em cards

--- src/components/CardOffers/CardOffers.js
+++ src/components/CardOffers/CardOffers.js
@@ -1,34 +1,10 @@
+import { promoCards } from '../../funcoes/promoCards';
 import Card from '../Card/Card';
 import '../CardOffers/CardOffers.css';
 
-const CardOffers = () => {
-    const promoCards = [
-        {
-            incluso: 'HOTEL+AÉREO',
-            destino: 'Japão',
-            preco: 4000,
-            background: require('../../assets/cards/jap-promo-card-bcg.png')
-        },
-        {
-            incluso: 'HOTEL+AÉREO',
-            destino: 'San Andreas',
-            preco: 3000,
-            background: require('../../assets/cards/sandreas-promo-card-bcg.png')
-        },
-        {
-            incluso: 'HOTEL+AÉREO',
-            destino: 'Paraíba',
-            preco: 1200,
-            background: require('../../assets/cards/paraiba-promo-card-bcg.png')
-        },
-        {
-            incluso: 'HOTEL+AÉREO',
-            destino: 'Manaus',
-            preco: 1600,
-            background: require('../../assets/cards/manaus-promo-card-bcg.png')
-        }
-    ];
 
+
+const CardOffers = () => {
     return (
         <section className='container'>
             <h2 className='container__subtitle' >Ofertas da semana</h2>
---
+++ src/funcoes/promoCards.js
@@ -0,0 +1,26 @@
+export const promoCards = [
+    {
+        incluso: 'HOTEL+AÉREO',
+        destino: 'Japão',
+        preco: 4000,
+        background: require('../assets/cards/jap-promo-card-bcg.png')
+    },
+    {
+        incluso: 'HOTEL+AÉREO',
+        destino: 'San Andreas',
+        preco: 3000,
+        background: require('../assets/cards/sandreas-promo-card-bcg.png')
+    },
+    {
+        incluso: 'HOTEL+AÉREO',
+        destino: 'Paraíba',
+        preco: 1200,
+        background: require('../assets/cards/paraiba-promo-card-bcg.png')
+    },
+    {
+        incluso: 'HOTEL+AÉREO',
+        destino: 'Manaus',
+        preco: 1600,
+        background: require('../assets/cards/manaus-promo-card-bcg.png')
+    }
+];
